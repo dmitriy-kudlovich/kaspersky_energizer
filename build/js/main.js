940bfb8eb4f0af2041a73d61012e1f2a3e95e6b9
Dmitriy Kudlovich <carnagery@mail.ru>
2021-03-13 23:56:22 +0300

    card ready

--- build/js/main.js
+++ build/js/main.js
@@ -3,22 +3,37 @@ const currencyValue = document.querySelector(".currency-menu__value");
 const currency = document.querySelectorAll(".currency-menu__item");
 
 // Выбор плана подписки
+// Для того, чтобы убрать список с ценами при клике не на него
 const html = document.querySelector("html");
-const planButton = document.querySelectorAll(".menu-plan__wrapper");
-const planList = document.querySelectorAll(".menu-plan__list");
+// Карточка продукта
 const productCard = document.querySelectorAll(".poduct-card");
+// Блок, в котором находится выпадающий список
+const planButton = document.querySelectorAll(".menu-plan__wrapper");
+// Выбранный план подписки
 const planValue = document.querySelectorAll(".menu-plan__value");
+// Список с ценами
+const planList = document.querySelectorAll(".menu-plan__list");
+// Элемент списка
 const plan = document.querySelectorAll(".menu-plan__list-item");
+// Цена подписки (из элемента списка)
 const planPrise = document.querySelectorAll(".menu-plan__prise");
+// Скидка
 const poductDiscountValue = document.querySelectorAll(
   ".poduct-card__discount-value"
 );
-const priseDisabled = document.querySelectorAll(".poduct-card__prise-disabled");
+// Цена без скидки
+const priseDisabled = document.querySelectorAll(".prise--disabled");
+// Выбранная цена
+const priseChosen = document.querySelectorAll(".poduct-card__prise--chosen");
+
+// Чек-боксы
+const cardCheckBoxes = document.querySelectorAll(".poduct-card__checkbox");
+const cardCheckBoxesLabels = document.querySelectorAll(".card__checkbox-lable");
 
 // Функция выбора значений из списка валюты
 function menuList(value, listItem) {
   listItem.forEach((item) => {
-    item.addEventListener("click", (event) => {
+    item.addEventListener("click", () => {
       value.innerHTML = item.innerHTML;
     });
   });
@@ -28,7 +43,7 @@ menuList(currencyValue, currency);
 // Функция выпадающего меню плана
 function planListActive() {
   planButton.forEach((item, index) => {
-    item.addEventListener("click", (event) => {
+    item.addEventListener("click", () => {
       planList[index].classList.toggle("list--active");
     });
 
@@ -49,6 +64,8 @@ function menuPlan(value, listItem) {
   let price = Number(planPrise[0].getAttribute("data-prise"));
   let discount = Math.round(price * 0.5);
   let oldPrice = Number((price + discount).toFixed(2));
+  let priseInt = parseInt(price); //Целая часть цены
+  let priseFloat = (price - priseInt).toFixed(2) * 100; //Дробная часть числа
 
   // Вывод значений, выбранных по умолчанию
   // Цена элемента списка, выбранного по умолчанию
@@ -61,7 +78,11 @@ function menuPlan(value, listItem) {
   });
   // Цена элемента списка, выбранного по умолчанию, без скидки
   priseDisabled.forEach((item) => {
-    item.innerHTML = "$" + oldPrice + "*";
+    item.innerHTML = "$" + oldPrice;
+  });
+  // Итоговая выбранная цена
+  priseChosen.forEach((item) => {
+    item.innerHTML = `<div class="row align-bottom"><sub class="card__prise--float">$</sub>${priseInt}</div><sup class="card__prise--float">,${priseFloat}*</sup>`;
   });
 
   // Расчёт и вывод при выборе значений из списка
@@ -69,6 +90,8 @@ function menuPlan(value, listItem) {
     item.addEventListener("click", () => {
       // Получение цены выбранного плана подписки
       let price = Number(planPrise[index].getAttribute("data-prise"));
+      let priseInt = parseInt(price); //Целая часть цены
+      let priseFloat = (price - priseInt).toFixed(2) * 100; //Дробная часть числа
 
       // Расчёт скидки
       let discount = Math.round(price * 0.5);
@@ -80,9 +103,26 @@ function menuPlan(value, listItem) {
       for (let i = 0; i < productCard.length; i++) {
         value[i].innerHTML = item.innerHTML;
         poductDiscountValue[i].innerHTML = "$" + discount + " OFF";
-        priseDisabled[i].innerHTML = "$" + oldPrice + "*";
+        priseDisabled[i].innerHTML = "$" + oldPrice;
+        priseChosen[
+          i
+        ].innerHTML = `<div class="row align-bottom"><sub class="card__prise--float">$</sub>${priseInt}</div><sup class="card__prise--float">,${priseFloat}*</sup>`;
       }
     });
   });
 }
 menuPlan(planValue, plan);
+
+// Чек-бокс
+// Присвоение атрибутов id и for чекбоксам в карточках
+for (let i = 0; i < cardCheckBoxes.length; i++) {
+  let itemId = "card__checkbox--" + i;
+  cardCheckBoxes[i].id = itemId;
+  cardCheckBoxesLabels[i].setAttribute("for", itemId);
+}
+
+cardCheckBoxes.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    cardCheckBoxes[index].classList.toggle("checkbox--checked");
+  });
+});
